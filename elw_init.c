b508a70ff784eeaa0c0eedb8fb63662aae4615ba
Sergio Prado <sergio.prado@e-labworks.com>
2023-03-19 10:19:36 -0300

    elw_init: initialize hal only one time
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- elw_init.c
+++ elw_init.c
@@ -1,6 +1,8 @@
 #include "elw_hal.h"
 #include "MKL46Z4.h"
 
+static int hal_initialized = 0;
+
 static void elw_irq_prio_init(void)
 {
     for (int i = 0; i < 32; i++)
@@ -9,13 +11,16 @@ static void elw_irq_prio_init(void)
 
 void elw_init_hal(void)
 {
-    elw_irq_prio_init();
-    elw_timer_init();
-    elw_led_init();
-    elw_btn_init();
-    elw_lcd_init();
-    elw_uart_init();
-    elw_accel_init();
-    elw_rtc_init();
-    elw_light_init();
+    if (!hal_initialized) {
+        elw_irq_prio_init();
+        elw_timer_init();
+        elw_led_init();
+        elw_btn_init();
+        elw_lcd_init();
+        elw_uart_init();
+        elw_accel_init();
+        elw_rtc_init();
+        elw_light_init();
+        hal_initialized = 1;
+    }
 }
